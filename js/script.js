370197bdf1ead5b3c4f861db58f05c7e72cee045
AKNL <aknlsame@gmail.com>
2024-02-09 16:35:04 +0100

    commit10

--- js/script.js
+++ js/script.js
@@ -4,24 +4,34 @@ let navbar = document.querySelector('.navbar');
 let sections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav a');
 
-window.onscroll = () => {
-    sections.forEach(sec => {
-        let top = windows.scrollY; 
-        let offset = sec.offsetTop - 150;
-        let height = sec.offsetHeight;
-        let id = sec.getAttribute('id');
 
-        if(top >= offset && top < offset + height) {
-            navLinks.forEach(links => {
-                links.classList.remove('active'); 
-                document.querySelector('header nav a [href*=' + id + '] ').classList.add('active');
-            })
-        }
-    })
-}
+// window.onscroll = () => {
+//     sections.forEach(sec => {
+//         let top = window.scrollY; 
+//         let offset = sec.offsetTop - 150;
+//         let height = sec.offsetHeight;
+//         let id = sec.getAttribute('id');
+        
+        
+
+//         if(top >= offset && top < offset + height) {
+//             navLinks.forEach(links => {
+
+//                 console.log(links);
+//                 links.classList.remove('active'); 
+//                 document.querySelector('header nav a [href*=' + id + '] ').classList.add('active');
+//             })
+//         }
+//     })
+// }
 
 menuIcon.onclick = () => {
     menuIcon.classList.toggle('bx-x');
-    navbar.classList.toggle('active');
+    navbar.style.display = 'block';
 
 }
+
+navbar.addEventListener("focusout" ,() => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.style.display = 'none';
+})
